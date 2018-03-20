a06eef5c12aee419fb4838bbeea7eb6f349397c9
Benji <benjifriedman@gmail.com>
2018-03-20 00:09:29 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+javascriptFunctions
+some helpful javascript functions
+
+
+/*   
+Here's some useful functions to convert / extract dictionaries to / from arrays 
+*/
+
+
+var testArray = ["one","two","three","apple","banana"];
+var testDict = {one:"one",two:"two",three:3,four:"lalala"};
+
+function makeArrayADictionaryOfValues(array) {
+    var dictionary = {};
+
+    for (var x in array) {
+        dictionary[x] = array[x];
+    }//for
+
+    return dictionary;
+}//makeArrayADictionaryOfValues
+
+//console.log(makeArrayADictionaryOfValues(testArray))
+
+function makeArrayADictionaryOfKeys(array) {
+    var dictionary = {};
+    for (var x in array) {
+        dictionary[array[x]] = null;
+    }
+    return dictionary;
+}//makeArrayADictionaryOfKeys
+
+//console.log(makeArrayADictionaryOfKeys(testArray))
+
+
+function makeDictionaryKeysAnArray(dictionary) {
+    var newArray = [];
+    for (var x in dictionary) {
+        newArray.push(x);
+    }
+    return newArray;
+}//makeDictionaryKeysAnArray
+
+//console.log(makeDictionaryKeysAnArray(testDict));
+
+
+
+function makeDictionaryValuesAnArray(dictionary) {
+    var newArray = [];
+    for (var x in dictionary) {
+        newArray[x] = dictionary[x];
+    }
+    return newArray;
+}//makeDictionaryValuesAnArray
+
+//console.log("the new array is: "+makeDictionaryValuesAnArray(testDict));
